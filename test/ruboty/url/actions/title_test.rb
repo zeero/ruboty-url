74ff22efe51c25a423997775b7a25c53e204d9fd
zeero <zeero26@gmail.com>
2018-03-17 11:42:07 +0900

    fix: ts is not required

--- test/ruboty/url/actions/title_test.rb
+++ test/ruboty/url/actions/title_test.rb
@@ -11,19 +11,16 @@ describe Ruboty::Url::Actions::Title do
   describe '#call' do
     let(:url) { 'https://foo/' }
     let(:title) { "foo's title" }
-    let(:timestamp) { Time.at(100) }
 
     it 'should get HTML title' do
       # stub
       mock_message.stubs(:[]).with(0).returns(url)
       OpenURI.stubs(:open_uri).yields(StringIO.new("<html><head><title>#{title}</title></head></html>"))
-      Time.stubs(:now).returns(timestamp)
       # mock
       attachments = [{
         color: '#0099ff',
         title: title,
         title_link: url,
-        ts: timestamp.to_i,
       }]
       mock_message.expects(:reply).with(nil, attachments: attachments).once
       subject.call
