be5c7ea2a97a3801da99ff93a88ff781e0d178f6
zeero <zeero26@gmail.com>
2018-03-18 17:34:11 +0900

    feat: add optional infos for attachment

--- lib/ruboty/url/actions/title.rb
+++ lib/ruboty/url/actions/title.rb
@@ -7,17 +7,27 @@ module Ruboty
       class Title < Ruboty::Actions::Base
         def call
           url = message[0]
-          title = ''
-          open(url) do |f|
-            doc = Nokogiri::HTML(f)
-            title = doc.xpath('/html/head/title').text
-          end
-          attachments = [{
+          attachment = {
             color: '#EEEEEE',
-            title: title,
             title_link: url,
-          }]
-          message.reply(nil, attachments: attachments) unless title.empty?
+          }
+          open(url) do |f|
+            html = Nokogiri::HTML(f)
+            attachment[:title] = html.xpath('/html/head/title').text
+
+            # optional infos
+            og_title = html.xpath('/html/head/meta[@property="og:title"]/@content').text
+            attachment[:title] = og_title if og_title.present?
+            og_site_name = html.xpath('/html/head/meta[@property="og:site_name"]/@content').text
+            attachment[:author_name] = og_site_name if og_site_name.present?
+            favicon_url = html.xpath('/html/head/link[@rel="icon"]/@href').text
+            attachment[:author_icon] = favicon_url if favicon_url.present?
+            og_description = html.xpath('/html/head/meta[@property="og:description"]/@content').text
+            attachment[:text] = og_description if og_description.present?
+            og_image = html.xpath('/html/head/meta[@property="og:image"]/@content').text
+            attachment[:thumb_url] = og_image if og_image.present?
+          end
+          message.reply(nil, attachments: [attachment]) if attachment[:title].present?
         end
       end
     end
--- test/ruboty/url/actions/title_test.rb
+++ test/ruboty/url/actions/title_test.rb
@@ -19,8 +19,32 @@ describe Ruboty::Url::Actions::Title do
       # mock
       attachments = [{
         color: '#EEEEEE',
-        title: title,
         title_link: url,
+        title: title,
+      }]
+      mock_message.expects(:reply).with(nil, attachments: attachments).once
+      subject.call
+    end
+
+    let(:og_title) { "bar's title" }
+    let(:og_site_name) { "bar's site name" }
+    let(:favicon_url) { "https://bar.site/favicon.ico" }
+    let(:og_description) { "bar's description." }
+    let(:og_image) { "https://bar.site/image.png" }
+
+    it 'should get optional infos' do
+      # stub
+      mock_message.stubs(:[]).with(0).returns(url)
+      OpenURI.stubs(:open_uri).yields(StringIO.new("<html><head><meta property=\"og:image\" content=\"#{og_image}\" /><meta property=\"og:site_name\" content=\"#{og_site_name}\" /><meta property=\"og:type\" content=\"object\" /><meta property=\"og:title\" content=\"#{og_title}\" /><meta property=\"og:url\" content=\"https://github.com/zeero/ruboty-url\" /><meta property=\"og:description\" content=\"#{og_description}\" /><title>#{title}</title><link rel=\"icon\" type=\"image/x-icon\" class=\"js-site-favicon\" href=\"#{favicon_url}\"></head></html>"))
+      # mock
+      attachments = [{
+        color: '#EEEEEE',
+        title_link: url,
+        title: og_title,
+        author_name: og_site_name,
+        author_icon: favicon_url,
+        text: og_description,
+        thumb_url: og_image,
       }]
       mock_message.expects(:reply).with(nil, attachments: attachments).once
       subject.call
